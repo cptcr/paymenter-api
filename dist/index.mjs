f735578b2774f24ce37a89708af2145501a75551
Cptcr <toowake@proton.me>
2025-02-09 07:42:11 +0100

    Add files via upload

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,30 +1,3 @@
-"use strict";
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-// src/index.ts
-var index_exports = {};
-__export(index_exports, {
-  Admin: () => Admin_default,
-  Client: () => Client_default
-});
-module.exports = __toCommonJS(index_exports);
-
 // src/functions/validate/validateDomain.ts
 function validateDomain(panel) {
   if (!panel.startsWith("https") || !panel.startsWith("http")) {
@@ -473,9 +446,8 @@ Admin.Invoice = class {
   }
 };
 var Admin_default = Admin;
-// Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {
-  Admin,
-  Client
-});
-//# sourceMappingURL=index.js.map
+export {
+  Admin_default as Admin,
+  Client_default as Client
+};
+//# sourceMappingURL=index.mjs.map
